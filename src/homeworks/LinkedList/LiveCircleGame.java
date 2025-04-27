e4554db939601e5d682f015e4dc6d670e8a9f2e1
baranov.v.a <baranov.v.a@eltl.ru>
2025-04-27 09:50:09 +0300

    hw_LinkedList: выполнить дз по LinkedList

--- src/homeworks/LinkedList/LiveCircleGame.java
+++ src/homeworks/LinkedList/LiveCircleGame.java
@@ -1,6 +1,13 @@
+package homeworks.LinkedList;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/**
+ * Задача. “Слова/Города” (Живой круг)
+ * Исправить все ошибки в коде, чтобы игра работала верно.
+ * Игра в слова/города, люди называют слова по кругу, если человек <дальше вы придумываете условия, при которых люди будут выбывать по какой-то причине, пока не останется один человек – победитель>
+ */
 public class LiveCircleGame {
     public static void main(String[] args) {
         LinkedList<String> players = new LinkedList<>();
@@ -8,41 +15,55 @@ public class LiveCircleGame {
 
         // Заполнение списка участников
         System.out.println("Введите имена участников игры (для завершения введите \"конец\"):");
-        String name;
-        while (!(name = scanner.nextLine()).equalsIgnoreCase("конец")) {
-            players.add(name);
+        // TODO: добавлено значение по умолчанию
+        String name = "";
+        while (!name.equalsIgnoreCase("конец")) {
+            // TODO: name перенесено внутрь из условия
+            name = scanner.nextLine();
+            // TODO: добавлено условие
+            if (!name.equals("конец")) {
+                players.add(name);
+            }
         }
 
         // Игровой процесс
         System.out.println("Игра начинается!");
         String lastWord = "";
-        while (!players.isEmpty()) {
-            String currentPlayer = players.poll(); // Получаем текущего игрока и убираем его из списка
+
+        // TODO: изменено условие
+        while (players.size() > 1) {
+            String currentPlayer = players.poll(); // Получаем текущего игрока и убираем его из списка - первый элемент из списка
             String inputWord;
 
             // Просим текущего игрока назвать слово, начинающееся с последней буквы предыдущего слова
             if (lastWord.isEmpty()) {
-                System.out.println(currentPlayer + ", назовите слово:");
+                System.out.println(currentPlayer + ", назовите слово: ");
             } else {
-                System.out.println(currentPlayer + ", назовите слово, начинающееся с буквы \"" + lastWord.charAt(lastWord.length() - 1) + "\":");
+                System.out.println(currentPlayer + ", назовите слово, начинающееся с буквы \"" + lastWord.charAt(lastWord.length() - 1) + "\": ");
             }
 
+            int count = 2;
             // Проверяем правильность введенного слова
             while (true) {
                 inputWord = scanner.nextLine().toLowerCase();
                 if (lastWord.isEmpty() || (lastWord.length() > 0 && inputWord.charAt(0) == lastWord.charAt(lastWord.length() - 1))) {
                     lastWord = inputWord; // Обновляем последнее слово
+                    // Добавляем текущего игрока в конец списка
+                    players.addLast(currentPlayer);
                     break;
                 } else {
-                    System.out.println("Неверное слово! Попробуйте еще раз:");
+                    if (count == 0) {
+                        System.out.println("Игрок " + currentPlayer + " выбыл из игры. Количество игроков " + players.size() + ".");
+                        break;
+                    } else {
+                        System.out.println("Неверное слово! Количество попыток " + count-- + ". Попробуйте еще раз:");
+                    }
                 }
             }
-
-            // Добавляем текущего игрока в конец списка
-            players.addLast(currentPlayer);
         }
 
+        // TODO: изменен вывод
         // Победитель
-        System.out.println("Поздравляем, " + lastWord + " - это последнее слово в игре!");
+        System.out.println("Поздравляем, " + players.getFirst() + " Вы победитель в игре!");
     }
 }
