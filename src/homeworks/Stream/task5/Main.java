c79ed891522b8f45a3e9362f00902502b7b5f3ba
baranov.v.a <baranov.v.a@eltl.ru>
2025-06-05 21:54:40 +0300

    hw-stream: add task 4-6

--- src/homeworks/Stream/task5/Main.java
+++ src/homeworks/Stream/task5/Main.java
@@ -1,42 +1,36 @@
 package homeworks.Stream.task5;
 
-import java.util.ArrayList;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.List;
 
-/*
-    Параллельная обработка:
-    - Создайте список чисел от 1 до 1000.
-    - Используя параллельный поток, найдите сумму квадратов всех чисел.
-    - Сравните время выполнения с обычным потоком.
+/**
+    Обработка данных из файла:
+    - Прочитайте содержимое текстового файла в список строк.
+    - Используя Stream, отфильтруйте только те строки, которые содержат определенное слово.
+    - Подсчитайте количество строк, соответствующих условию.
  */
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= 1_000; i++) {
-            numbers.add(i);
+        List<String> listForWriting  = Arrays.asList("task1", "task2", "task2", "task4", "task2", "task6");
+        Path path = Paths.get("./data/listStrings.txt");
+
+        try {
+            Files.write(path, listForWriting, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+            List<String> listReading = Files.readAllLines(path, StandardCharsets.UTF_8);
+            System.out.println("Список полученный из файла");
+            listReading.forEach(System.out::println);
+            List<String> listReadingFiltered = listReading.stream().filter(w -> w.equals("task2")).toList();
+            System.out.println("Список полученный из файла после фильтрации");
+            listReadingFiltered.forEach(System.out::println);
+            System.out.println("Количество отфильтрованных значений " + listReadingFiltered.size());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-        long start = System.currentTimeMillis();
-        long sum = numbers.parallelStream()
-                .reduce(
-                        0L,
-                        (subtotal, element) -> subtotal + ((long) element * element),
-                        Long::sum
-                );
-        System.out.println("Сумма вычисленная в параллельных потоках - " + sum);
-        long end = System.currentTimeMillis();
-        System.out.println("Время вычисления в параллельных потоках, мс - " + (end - start));
-
-        start = System.currentTimeMillis();
-        sum = numbers.stream()
-                .reduce(
-                        0L,
-                        (subtotal, element) -> subtotal + ((long) element * element),
-                        Long::sum
-                );
-        System.out.println("Сумма вычисленная в одном потоке - " + sum);
-        end = System.currentTimeMillis();
-        System.out.println("Время вычисления в одном потоке, мс - " + (end - start));
-
     }
 }
