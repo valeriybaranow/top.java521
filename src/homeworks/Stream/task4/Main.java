7696e99cf0351079828fe3febeebcc545c3cbaaf
baranov.v.a <baranov.v.a@eltl.ru>
2025-06-05 21:54:40 +0300

    hw-stream: add task 4-6

--- src/homeworks/Stream/task4/Main.java
+++ src/homeworks/Stream/task4/Main.java
@@ -1,36 +1,27 @@
 package homeworks.Stream.task4;
 
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
-/**
-    Обработка данных из файла:
-    - Прочитайте содержимое текстового файла в список строк.
-    - Используя Stream, отфильтруйте только те строки, которые содержат определенное слово.
-    - Подсчитайте количество строк, соответствующих условию.
+
+/*
+    Сортировка и поиск:
+    - Создайте список целых чисел.
+    - Отсортируйте его по убыванию.
+    - Найдите первые 3 элемента после сортировки.
  */
 public class Main {
     public static void main(String[] args) {
-        List<String> listForWriting  = Arrays.asList("task1", "task2", "task2", "task4", "task2", "task6");
-        Path path = Paths.get("./data/listStrings.txt");
+        List<Integer> list = Arrays.asList(1,2,3,4,6);
 
-        try {
-            Files.write(path, listForWriting, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
-            List<String> listReading = Files.readAllLines(path, StandardCharsets.UTF_8);
-            System.out.println("Список полученный из файла");
-            listReading.forEach(System.out::println);
-            List<String> listReadingFiltered = listReading.stream().filter(w -> w.equals("task2")).toList();
-            System.out.println("Список полученный из файла после фильтрации");
-            listReadingFiltered.forEach(System.out::println);
-            System.out.println("Количество отфильтрованных значений " + listReadingFiltered.size());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+       System.out.println(list);
+
+        List<Integer> changedList = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(3).toList();
+
+        System.out.println("Список после преобразования");
+        System.out.println(changedList);
     }
 }
